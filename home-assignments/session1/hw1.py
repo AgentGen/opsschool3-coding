3ac50f493468eebb9838f9bbe5b0901cc7d47c42
agentgen <agent.gen@gmail.com>
2018-11-07 18:33:39 +0200

    Improvements

--- home-assignments/session1/hw1.py
+++ home-assignments/session1/hw1.py
@@ -1,23 +1,28 @@
+#!/usr/bin/python
 import json
-fo = open("names.yson", "wb")
-with open('names.json') as json_data:
-    d = json.load(json_data)
-ppl = (d["ppl_ages"])
-buckets = sorted(d["buckets"])
 
-a = 1
-for i in buckets:
-    if a != len(buckets):
-        print('Age range', buckets[a - 1], "to", buckets[a], file=open("names.yson", "a"))
-        for k, v in sorted(ppl.items(), key=lambda p: p[1], reverse=False):
-            if v <= buckets[a]:
-                print(k, file=open("names.yson", "a"))
-                del ppl[k]
-        if a <= (len(buckets) - 1):
-            a += 1
-z = sorted(ppl.items(), key=lambda p: p[1], reverse=True)[0]
-print('Age range', buckets[a-1], "to", z[1])
-for k, v in sorted(ppl.items(), key=lambda p: p[1], reverse=False):
+
+def sort_json_list(filename):
+
+    fo = open("names.yson", "wb")
+    with open(filename) as json_data:
+        d = json.load(json_data)
+    ppl = (d["ppl_ages"])
+    buckets = sorted(d["buckets"])
+    a = 1
+    for i in buckets:
+        if a != len(buckets):
+            print('Age range', buckets[a - 1], "to", buckets[a], file=open("names.yson", "a"))
+            for k, v in sorted(ppl.items(), key=lambda p: p[1], reverse=False):
+                if v <= buckets[a]:
+                    print(k, file=open("names.yson", "a"))
+                    del ppl[k]
+            if a <= (len(buckets) - 1):
+                a += 1
+    z = sorted(ppl.items(), key=lambda p: p[1], reverse=True)[0]
+    print('Age range', buckets[a-1], "to", z[1], file=open("names.yson", "a"))
+    for k, v in sorted(ppl.items(), key=lambda p: p[1], reverse=False):
         print(k, file=open("names.yson", "a"))
-        del ppl[k]
-fo.close()
+    del ppl[k]
+    fo.close()
+    print("Sorting complete")
